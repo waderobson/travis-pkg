8b1bac9e3b3de3b33eda327f764d06327d4ca1fb
Wade Robson <wade.robson4@gmail.com>
2017-03-20 22:47:16 -0700

    chmod derp

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
-chown +x scripts/
+chmod +x scripts/
 mkdir build
 if [ -z echo $TRAVIS_TAG ]; then
 pkgbuild --root ROOT --scripts scripts --identifier com.example.pkg --version $(echo $TRAVIS_TAG | cut -c2-) build/example.pkg
