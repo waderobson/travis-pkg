b62ff055ac23a99398447bc856c924c7b7bf3da6
Wade Robson <wade.robson4@gmail.com>
2017-03-20 23:06:59 -0700

    getting version 0.5 ready 👍

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 
-chmod +x scripts/
+chmod  -R +x scripts/
 mkdir build
+
 if [ ! -z $TRAVIS_TAG ]; then
-pkgbuild --root ROOT --scripts scripts --identifier com.example.pkg --version $(echo $TRAVIS_TAG | cut -c2-) build/example.pkg
+VERSION=$(echo $TRAVIS_TAG | cut -c2-)
+  echo "building package $VERSION"
+  pkgbuild --root ROOT --scripts scripts --identifier com.example.pkg --version $VERSION build/example.pkg
 fi
