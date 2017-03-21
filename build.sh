ff7a52137ee86f65b43d2f7104c9a5bf77e94612
Wade Robson <wade.robson4@gmail.com>
2017-03-20 22:50:15 -0700

    please work

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 chmod +x scripts/
 mkdir build
-if [ -z echo $TRAVIS_TAG ]; then
+if [ ! -z $TRAVIS_TAG ]; then
 pkgbuild --root ROOT --scripts scripts --identifier com.example.pkg --version $(echo $TRAVIS_TAG | cut -c2-) build/example.pkg
 fi
