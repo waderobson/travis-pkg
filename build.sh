54b2f326a710cbe023fac9b794f8ea14d8592fc7
Wade Robson <wade.robson4@gmail.com>
2017-03-20 22:45:47 -0700

    build dir is back

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 chown +x scripts/
+mkdir build
 if [ -z echo $TRAVIS_TAG ]; then
 pkgbuild --root ROOT --scripts scripts --identifier com.example.pkg --version $(echo $TRAVIS_TAG | cut -c2-) build/example.pkg
 fi
